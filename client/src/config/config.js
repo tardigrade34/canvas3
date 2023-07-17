8d7daf1bd428b8053deb4ddbefaceaf0c4c4b3f0
tardigrade34 <muharremacar38@gmail.com>
2023-07-17 21:24:39 +0300

    api corrected

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -3,7 +3,7 @@ const config = {
     backendUrl: "http://localhost:8080/api/v1/dalle",
   },
   production: {
-    backendUrl: "https://canvas3.onrender.com/api/v1/dalle",
+    backendUrl: "https://devswag.onrender.com/api/v1/dalle",
   },
 };
 
